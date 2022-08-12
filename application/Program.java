ba93eb6810bcc59d409283d316bc98ca0c7a43a3
mdorici <marianadorici@gmail.com>
2022-08-12 19:10:36 -0300

    First commit

---
+++ application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import model.entities.Account;
+import model.exceptions.BusinessException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        double initialBalance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        double amount = sc.nextDouble();
+
+        try {
+            account.withdraw(amount);
+            System.out.printf("New balance: %.2f%n", account.getBalance());
+        }
+        catch (BusinessException be) {
+            System.out.print("Withdraw error: " + be.getMessage());
+        }
+        catch (RuntimeException re) {
+            System.out.println("Unexpected error");
+        }
+
+        sc.close();
+    }
+}
